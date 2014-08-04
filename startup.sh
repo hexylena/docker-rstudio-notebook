6b890c24a6601942002fb4304360075cdd3c5160
Eric Rasche <rasche.eric@yandex.ru>
2014-08-04 13:11:28 -0500

    Working startup/shutdown scripts

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /etc/init.d/cron start
+CONF_FILE="/import/conf.yaml"
 USERNAME=$(grep 'notebook_username' ${CONF_FILE} | sed 's/notebook_username: //g')
 PASSWORD=$(grep 'notebook_password' ${CONF_FILE} | sed 's/notebook_password: //g')
 useradd -d /import/ -p $PASSWORD $USERNAME
