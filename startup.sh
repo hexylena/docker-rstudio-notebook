6b16ceab2c43a733d0f578c7f994f41b0b21d086
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-08-09 11:03:37 -0500

    Modified startup file to allow for pub-key save
    Additionally modified to allow for different method of passing user password which had presented
    locale issues.

--- startup.sh
+++ startup.sh
@@ -5,6 +5,14 @@ CONF_FILE="/import/conf.yaml"
 # Add the user
 USERNAME=$(grep 'notebook_username' ${CONF_FILE} | sed 's/notebook_username: //g')
 PASSWORD=$(grep 'notebook_password' ${CONF_FILE} | sed 's/notebook_password: //g')
-useradd -d /import/ -p $PASSWORD $USERNAME
+
+useradd -p `openssl passwd -1 $PASSWORD` $USERNAME -d /import/
+chown $USERNAME:$USERNAME /import/ -R
 # Start the server
-/usr/lib/rstudio-server/bin/rserver --server-daemonize 0
+#/usr/lib/rstudio-server/bin/rserver --server-daemonize 0
+service rstudio-server start
+sleep 1
+curl localhost:8787/auth-public-key > /import/rserver_pub_key
+chmod 777 /import/ -R
+# TODO implement something useful -- is it worht the install rsyslog?
+tail -f /var/log/dmesg
