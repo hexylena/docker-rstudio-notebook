ec5b43206e75e842889931e177a9bb1968935d5d
Eric Rasche <esr@tamu.edu>
2015-05-14 10:03:09 -0500

    Doc improvements

--- startup.sh
+++ startup.sh
@@ -16,20 +16,24 @@ cp /proxy.conf /etc/nginx/sites-enabled/default
 uid=`stat --printf %u /import`
 gid=`stat --printf %g /import`
 
+# If the group doesn't exist, add it
 [ $(getent group $gid) ] || groupadd -r galaxy -g $gid
+# Add the user (maybe not fault tolerant for existing UIDs)
 useradd -u $uid -r -g $gid -d /import \
     -c "RStudio Galaxy user" \
     -p `openssl passwd -1 $NOTEBOOK_PASSWORD` galaxy
+# Correct permissions on the folder
 chown $uid:$gid /import -R
 
 # Start the servers
 service rstudio-server start
 service nginx restart
 
-# Persist the environment elsewhere in the FS
+# RStudio users don't get the system environment for some reason (I need to
+# figure out a better way to fix this...). Right now we persist the environment
+# in a file which galaxy.py reads which is a little bit clunky and RStudio
+# specific
 env > /etc/profile.d/galaxy.sh
 
-# Chown import as that user so they can write there
-chown galaxy:galaxy /import/ -R
 chmod 770 /import/ -R
 tail -f /var/log/nginx/*
